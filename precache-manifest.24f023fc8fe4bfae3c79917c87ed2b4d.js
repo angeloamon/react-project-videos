bea03be4bc65136102842e1e88655ba610f5c32c
coffeemaker30 <angeloamon@gmail.com>
2019-05-15 09:07:38 +0800

    Updates

---
+++ precache-manifest.24f023fc8fe4bfae3c79917c87ed2b4d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad56b2d1b5fdf358fd5f9446cbf04be8",
+    "url": "/react-project-videos/index.html"
+  },
+  {
+    "revision": "95f4f4d26ca6549c06da",
+    "url": "/react-project-videos/static/css/main.03c6ca28.chunk.css"
+  },
+  {
+    "revision": "7e6ac1444ef9242ebc00",
+    "url": "/react-project-videos/static/js/2.824d5390.chunk.js"
+  },
+  {
+    "revision": "95f4f4d26ca6549c06da",
+    "url": "/react-project-videos/static/js/main.bbd2a70e.chunk.js"
+  },
+  {
+    "revision": "d1992d52ecd1dd855c82",
+    "url": "/react-project-videos/static/js/runtime~main.669d3136.js"
+  }
+]);
